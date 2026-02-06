c0834add32f62484b3cba5b4d9f9ddad5bd63922
HUTCHINSJO <onas28@gmail.ocm>
2026-02-06 13:41:47 -0500

    added line to main

--- src/main/java/edu/gcc/comp350/Driver.java
+++ src/main/java/edu/gcc/comp350/Driver.java
@@ -2,8 +2,9 @@ package edu.gcc.comp350;
 
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("hello jar");
+        System.out.println("hello jars");
         Calculator c = new Calculator();
         System.out.println(c.add(4,5));
+        System.out.println("jdhljfhl");
     }
 }
